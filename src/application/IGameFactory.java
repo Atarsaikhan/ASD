360377c98c57cbaa623964f66c7b8e5e7659af5e
Atarsaikhan <aaltangerel@mum.edu>
2019-02-02 10:09:49 -0600

    haslaa

--- src/application/IGameFactory.java
+++
@@ -1,7 +0,0 @@
-package application;
-
-import framework.IGameController;
-
-public interface IGameFactory {
-	public IGameController createGame(String type, String playerName1, String playerName2);
-}
--- src/application/IObserverMoveNumber.java
+++
@@ -1,5 +0,0 @@
-package application;
-
-public interface IObserverMoveNumber {
-	public void update(int moveNumber);
-}
